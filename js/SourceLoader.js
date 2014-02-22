4d8aafb4b7746de5600e8040b24498a636356196
lo-th <3dflashlo@gmail.com>
2014-02-22 12:50:46 +0100

    new test

--- js/SourceLoader.js
+++ js/SourceLoader.js
@@ -1,25 +1,53 @@
 var img = new Image();
+var scripts = [];
+
 img.onload = function() {
-	var c = document.createElement("canvas"), r='', pix;
+	var LoadInfo = "";
+	var t01 = Date.now();
+	var c=document.createElement("canvas"), r='', pix, i;
 	c.width = c.height = this.width;
 	c.getContext('2d').drawImage(this, 0, 0);
 	var d = c.getContext('2d').getImageData(0, 0, c.width, c.height).data;
-	for (var i = 0, l=d.length; i<l; i+=4){
+	var newScript = false;
+	var ns = 0;
+	var src = [];
+
+	for ( i = 0, l=d.length; i<l; i+=4){
 		pix = d[i];
-		if(pix>31 && pix<128 ) r+=String.fromCharCode(pix);
+		if(pix === 128) newScript = true;
+		if( pix>31 && pix<128 ){ 
+			if(newScript){ ns++; newScript = false; src[ns] ="";}
+            src[ns] += String.fromCharCode(pix);
+		}
+	}
+	var name, nn, pn, tmpsrc, lib=0;
+	for( i = 0; i<ns; i++){
+		tmpsrc = src[i+1];
+		src[i+1] = "";
+		nn = tmpsrc.indexOf("var");
+		pn = tmpsrc.indexOf("=");
+		name = tmpsrc.substring(nn+4,pn);
+		if(name === "={fromGeometry:function(n,t){var ") name = "TOOLS";
+		if(name === "e"){ if(lib===0){ lib++; name = "TWEENLITE";}  else {name = "HOWLER";}}
+
+		LoadInfo += name + ", ";
+		scripts[i] = document.createElement("script");
+		scripts[i].type = "text/javascript";
+		scripts[i].id = name;
+		scripts[i].charset = "utf-8";
+		scripts[i].async = true;
+		
+		scripts[i].textContent = tmpsrc;
+
+		document.getElementsByTagName('head')[0].appendChild(scripts[i]);
+		//document.body.appendChild(scripts[i]);
 	}
 
-	nscript = document.createElement("script");
-	nscript.type = "text/javascript";
-	nscript.name = "topScript";
-	nscript.id = "topScript";
-	nscript.charset = "utf-8";
-	nscript.async = true;
-	nscript.text = r;
-	document.getElementsByTagName('head')[0].appendChild(nscript);
+	tmpsrc = "";
+	src = null;
 
-	img = null;
+	LoadInfo += "<br>decode time: " + (Date.now()-t01) + " ms<br>";
 
-	sourceLoaded();
+	sourceLoaded(LoadInfo);
 };
-img.src = "img/oimo_three.png";
+img.src = "img/full.png";
